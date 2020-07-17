9570c6a5b5739c75f385247ac7ad1e9d4df1c572
André Lins <andrelucas01@hotmail.com>
2020-07-16 23:20:30 -0300

    New details in the package.json

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import fastify, { FastifyRequest } from 'fastify';
 import { ApolloServer, IResolvers } from 'apollo-server-fastify';
 import { ApolloServerBase, Context, Config } from 'apollo-server-core';
 import { DocumentNode } from 'graphql';
-import { Server } from 'http';
 
 type CreateMiddlewareParams = {
   typeDefs: DocumentNode;
@@ -20,12 +19,6 @@ class RecifeFastify {
   app = fastify({ logger: true });
 
   constructor(bodyParser: any, cors: any, homepage: string) {
-    // this.app.use(koaBody(bodyParser));
-
-    // if (cors && cors.enabled) {
-    //   this.app.use(koaCors(cors));
-    // }
-
     this.app.get('/', (_, reply) => {
       reply.send(homepage);
     });
